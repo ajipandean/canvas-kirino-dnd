1f9c116273237977df6685da5a7e38353efe916f
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-18 16:29:16 +0800

    Add react-konva lib

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Button, Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-      <Navbar bg="light" expand="sm">
-        <Navbar.Brand href="#">Drawing Hooks Using Canvas API</Navbar.Brand>
+      <Navbar
+        bg="white"
+        fixed="top"
+        expand="sm"
+        className="border-bottom"
+      >
+        <Navbar.Brand href="#">Drawing Using React Konva</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Button variant="light">Undo</Button>
-            <Button variant="light">Clear</Button>
+            <Nav.Link>Undo</Nav.Link>
+            <Nav.Link>Clear</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <canvas/>
     </>
   );
 }
